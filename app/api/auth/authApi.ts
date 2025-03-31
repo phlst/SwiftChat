e1ecaac787eaffc9f4c5b6515b8f4368fde9d515
phlst <hajdu2024@gmail.com>
2025-03-31 20:15:55 +0200

    Working Layout mostly

--- app/api/auth/authApi.ts
+++ app/api/auth/authApi.ts
@@ -5,11 +5,3 @@ import { signIn } from "@/app/auth";
 export async function signInGoogle() {
   await signIn("google");
 }
-
-export async function signInFacebook() {
-  await signIn("facebook");
-}
-
-export async function signInGithub() {
-  await signIn("github");
-}

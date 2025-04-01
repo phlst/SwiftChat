eda0a1a8fe73dc083a0bae2fa86c76dc00e28352
phlst <hajdu2024@gmail.com>
2025-04-01 22:03:33 +0200

    Working on this stupid shit

--- app/lib/db/appwrite.ts
+++ app/lib/db/appwrite.ts
@@ -1,22 +1,35 @@
 "use server";
-import { Client, Account, ID, OAuthProvider } from "node-appwrite";
+import { Client, Account, ID } from "node-appwrite";
 
-// Initialize the Appwrite client
-const client = new Client();
-
-const ENDPOINT = process.env.PUBLIC_APPWRITE_ENDPOINT;
-const PROJECT_ID = process.env.PUBLIC_APPWRITE_PROJECT;
-const API_KEY = process.env.APPWRITE_API_KEY;
-
-client
-  .setEndpoint(ENDPOINT) // Your Appwrite Endpoint
-  .setKey(API_KEY)
-  .setProject(PROJECT_ID); // Your project ID
-
-const account = new Account(client);
+const ENDPOINT = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string;
+const PROJECT_ID = process.env.NEXT_PUBLIC_APPWRITE_PROJECT as string;
+const API_KEY = process.env.NEXT_APPWRITE_API_KEY as string;
 
 function isString(value: FormDataEntryValue | null): value is string {
   return typeof value === "string";
+}
+
+function createAdminClient() {
+  const client = new Client()
+    .setEndpoint(ENDPOINT)
+    .setProject(PROJECT_ID)
+    .setKey(API_KEY);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
+}
+
+function createSession() {
+  const client = new Client().setEndpoint(ENDPOINT).setProject(PROJECT_ID);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
 }
 
 export async function createUser(formData: FormData) {
@@ -26,6 +39,7 @@ export async function createUser(formData: FormData) {
     const name = formData.get("name");
 
     if (isString(email) && isString(password) && isString(name)) {
+      const { account } = await createAdminClient();
       const user = await account.create(ID.unique(), email, password, name);
       console.log("User created successfully:", user);
     } else {
@@ -39,6 +53,7 @@ export async function createUser(formData: FormData) {
 export async function logInUser(formData: FormData) {
   try {
     const email = formData.get("email");
+    const { account } = createAdminClient();
     const password = formData.get("password");
     if (isString(email) && isString(password)) {
       const user = await account.createEmailPasswordSession(email, password);
@@ -50,3 +65,5 @@ export async function logInUser(formData: FormData) {
     console.error("Error logging in:", error);
   }
 }
+
+// export async function logInGoogle() {}
